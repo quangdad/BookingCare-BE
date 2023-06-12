48e284caa3a70a2e36992d96b81ed9062c6a22f0
Le Dat <bapxanh02@gmail.com>
2023-06-12 14:32:00 +0700

    3.1

--- src/route/web.js
+++ src/route/web.js
@@ -12,12 +12,12 @@ let initWebRoutes = (app) => {
   router.get("/edit-crud", homeController.getEditCRUD);
   router.post("/update-crud", homeController.updateCRUD);
   router.get("/delete-crud", homeController.deleteCRUD);
+
   router.post("/api/login", userController.handleLogin);
   router.get("/api/get-all-users", userController.handleGetAllUsers);
-
-  // router.get("/home", (req, res) => {
-  //   return res.send("Welcome");
-  // });
+  router.post("/api/create-new-user", userController.handleCreateNewUser);
+  router.put("/api/edit-user", userController.handleEditUser);
+  router.delete("/api/delete-user", userController.handleDeleteUser);
 
   return app.use("/", router);
 };
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,6 +1,17 @@
-import db from "../models/index";
 import bcrypt from "bcryptjs";
+import db from "../models/index";
+const salt = bcrypt.genSaltSync(10);
 
+let hashUserPassword = (passWord) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let hashPassword = await bcrypt.hashSync(passWord, salt);
+      resolve(hashPassword);
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
 let handleUserLogin = (email, password) => {
   return new Promise(async (resolve, reject) => {
     try {
@@ -82,14 +93,103 @@ let getAllUsers = (userId) => {
         });
       }
       resovle(user);
+      console.log(user);
     } catch (e) {
       reject(e);
     }
-    // console.log(user);
+  });
+};
+
+let createNewUser = async (data) => {
+  console.log("test: ", data);
+
+  return new Promise(async (resovle, reject) => {
+    try {
+      let check = await checkUserEmail(data.email);
+      if (check === true) {
+        resovle({
+          err: 1,
+          mes: "Your email is already in used!",
+        });
+      } else {
+        let hashPasswordFromBcrypt = await hashUserPassword(data.passWord);
+        await db.User.create({
+          email: data.email,
+          password: hashPasswordFromBcrypt,
+          firstName: data.firstName,
+          lastName: data.lastName,
+          address: data.address,
+          phonenumber: data.phoneNumber,
+          gender: data.gender === "1" ? true : false,
+          roleID: data.role,
+        });
+        resovle({
+          err: 0,
+          mes: "OK",
+        });
+      }
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+let deleteUser = async (userId) => {
+  return new Promise(async (resolve, reject) => {
+    let user = await db.User.findOne({
+      where: { id: userId },
+    });
+    if (!user) {
+      resolve({
+        err: 2,
+        mes: "User isn't exist",
+      });
+    }
+    await db.User.destroy({
+      where: { id: userId },
+    });
+    resolve({
+      err: 0,
+      mes: "The user has been deleted successfully",
+    });
+  });
+};
+
+let updateUser = async (putData) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      if (!putData.id) {
+        console.log("check data nodejs: ", putData);
+        resolve({
+          err: 2,
+          mes: "User isn't exist",
+        });
+      }
+      let user = await db.User.findOne({
+        where: { id: putData.id },
+        raw: false,
+      });
+      if (user) {
+        user.firstName = putData.firstName;
+        user.lastName = putData.lastName;
+        user.address = putData.address;
+        user.phonenumber = putData.phoneNumber;
+        await user.save();
+        resolve({
+          err: 0,
+          mes: "The user has been updated successfully",
+        });
+      }
+    } catch (e) {
+      reject(e);
+    }
   });
 };
 
 module.exports = {
   handleUserLogin: handleUserLogin,
   getAllUsers: getAllUsers,
+  createNewUser: createNewUser,
+  deleteUser: deleteUser,
+  updateUser: updateUser,
 };
